8500f0a079d0f49eca381cf6ba83580b1a95c058
Himanshu Verma <hvinprimary@gmail.com>
2025-05-25 21:36:05 +0530

    add force full page reload to avoid rsc payload to render in the login page when user cancells the login from auth callback url

--- src/app/auth/confirm/callback/route.ts
+++ src/app/auth/confirm/callback/route.ts
@@ -7,8 +7,11 @@ export async function GET(request: Request) {
   const code = searchParams.get('code')
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get('next') ?? '/chat'
+  const redirect = (url: string) => {
+    return NextResponse.redirect(url, { status: 302 }) // ✅ Force full-page load
+  }
   if(!code){
-    return NextResponse.redirect(`${origin}/login?message=Login cancelled or failed`)
+    return redirect(`${origin}/login?message=Login cancelled or failed`)
   }
   if (code) {
     const supabase = await createClient()
@@ -18,11 +21,11 @@ export async function GET(request: Request) {
       const isLocalEnv = process.env.NODE_ENV === 'development'
       if (isLocalEnv) {
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${origin}${next}`)
+        return redirect(`${origin}${next}`)
       } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`)
+        return redirect(`https://${forwardedHost}${next}`)
       } else {
-        return NextResponse.redirect(`${origin}${next}`)
+        return redirect(`${origin}${next}`)
       }
     }
   }
